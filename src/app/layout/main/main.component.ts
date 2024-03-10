a100e913700eae64b01a56ceaa8a1f19c01157eb
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-03-10 01:11:23 +0100

    add main ui

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FileInputComponent } from '../../components/file-input/file-input.component';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [FileInputComponent, ButtonComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
+  images: File[] = [];
 }
